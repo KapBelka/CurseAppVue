946b515b66840b7f1bf4123ef440c85a8ae2ac59
Artem Khriplivec <belka1arri@gmail.com>
2025-04-25 14:05:23 +0700

    add register

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,11 @@ import { createWebHistory, createRouter } from 'vue-router'
 import LoginPage from './pages/login-page/login-page.vue'
 import ProjectPage from './pages/project-page/view/project-page.vue'
 import ProjectsListPage from './pages/project-page/view/projects-list-page.vue'
+import RegisterPage from './pages/register-page/register-page.vue'
 
 const routes = [
   { path: '/login', component: LoginPage },
+  { path: '/register', component: RegisterPage },
   { path: '/', component: ProjectsListPage },
   { path: '/project/:id', component: ProjectPage },
 ]
--- src/pages/register-page/register-page.vue
+++ src/pages/register-page/register-page.vue
@@ -4,7 +4,7 @@
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header px-4">
-            <h5 class="modal-title">Авторизация</h5>
+            <h5 class="modal-title">Регистрация</h5>
           </div>
           <div class="modal-body px-4">
             <div class="mb-3">
@@ -17,6 +17,24 @@
               />
             </div>
             <div class="mb-3">
+              <label for="nameInput" class="form-label">Имя</label>
+              <input
+                v-model="name"
+                type="text"
+                class="form-control"
+                id="nameInput"
+              />
+            </div>
+            <div class="mb-3">
+              <label for="surnameInput" class="form-label">Фамилия</label>
+              <input
+                v-model="surname"
+                type="text"
+                class="form-control"
+                id="surnameInput"
+              />
+            </div>
+            <div class="mb-3">
               <label for="passwordInput" class="form-label">Пароль</label>
               <input
                 v-model="password"
@@ -25,19 +43,32 @@
                 id="passwordInput"
               />
             </div>
+            <div class="mb-3">
+              <label for="secondPasswordInput" class="form-label">Повтор пароля</label>
+              <input
+                v-model="secondPassword"
+                type="password"
+                class="form-control"
+                id="secondPasswordInput"
+                :class="{'is-invalid': secondPassword != password}"
+              />
+              <div class="invalid-feedback">
+                Пароли не совпадают
+              </div>
+            </div>
           </div>
           <div class="modal-footer px-4">
-            <button class="btn btn-primary c-pointer ms-2" @click="$router.push('/register')">
-                Регистрация
+            <button class="btn btn-primary c-pointer ms-2" @click="$router.push('/login')">
+                Вход
             </button>
-            <button :disabled="!canLogin" class="btn btn-primary c-pointer ms-2" @click="login">
+            <button :disabled="!canRegister" class="btn btn-primary c-pointer ms-2" @click="register">
                 <span
                 v-show="loading"
                 class="spinner-border spinner-border-sm"
                 role="status"
                 aria-hidden="true"
                 ></span>
-                Войти
+                Зарегистрироваться
             </button>
           </div>
         </div>
@@ -52,7 +83,10 @@ import AuthService from "../../services/auth/auth-service";
 
 interface Data {
   emailAddress: string | null;
+  name: string | null;
+  surname: string | null;
   password: string | null;
+  secondPassword: string | null;
   loading: boolean;
   service: AuthService;
 }
@@ -64,17 +98,20 @@ export default defineComponent({
       loading: false,
       service: new AuthService(),
       emailAddress: null,
-      password: null
+      password: null,
+      secondPassword: null
     } as Data;
   },
   methods: {
-    async login() {
-      if (!this.canLogin) return;
+    async register() {
+      if (!this.canRegister) return;
 
       this.loading = true
-      var response = await this.service.Login({
+      var response = await this.service.Register({
         emailAddress: this.emailAddress!,
         password: this.password!,
+        name: this.name!,
+        surname: this.surname!
       });
       this.loading = false
 
@@ -86,8 +123,8 @@ export default defineComponent({
     },
   },
   computed: {
-    canLogin(): boolean {
-      return !!this.emailAddress && !!this.password;
+    canRegister(): boolean {
+      return !!this.emailAddress && !!this.password && !!this.name && !!this.surname && !!this.secondPassword && this.password == this.secondPassword;
     },
   },
   watch: {},
